e8e207b625188aaf9322a67b2876645f4af8eeaa
raj <rajk80007@gmail.com>
2024-05-10 18:55:30 +0530

    footer completed

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -34,11 +34,11 @@ const Header = () => {
         <div className='flex items-center gap-10'>
 
         <Image src="/logo.svg" alt="logo" width={170} height={80} />
-        <ul className={open? 'flex flex-col absolute top-20 gap-6 justify-center items-center ml-[-2.8%] w-[100%] z-40 md:hidden text-2xl font-bold bg-blue-100 text-blue-950 bg-opacity-80 py-5 rounded-lg':'gap-8 hidden md:flex' } >
+        <ul className={open? 'flex flex-col absolute top-20 gap-6 justify-center items-center ml-[-2.8%] w-[100%] z-40 md:hidden text-[1.2rem] font-bold bg-blue-100 text-blue-950 bg-opacity-80 py-2 rounded-lg pb-6':'gap-8 hidden md:flex' } >
            {Menu.map((item,index)=>(
             <Link href={item.path} key={index}>
                <li 
-               className='cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out'>{item.nmae}</li>
+               className='cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out rounded-full'>{item.nmae}</li>
                </Link>
             ))}
         </ul>
@@ -47,9 +47,10 @@ const Header = () => {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-round"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg>
             </Link>
             <ul onMouseOver={()=>setUserOpen(true)} onMouseOut={()=>setUserOpen(false)} className={userOpen?'shadow-lg z-50 absolute pl-4 pr-16 right-4 top-[11.4%] bg-red-300 rounded-sm':'hidden'}>
-                <Link href='#'><li className='py-2 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out'>Dashboard </li></Link>
-                <Link href='#'><li className='pb-2 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out'>Account </li></Link>
-                <Link href='#'><li className='pb-2 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out'>Logout</li></Link>
+                <Link href='/'><li className='py-1 my-1 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out hover:bg-white hover:px-1 hover:rounded-sm'>Dashboard </li></Link>
+                <Link href='#'><li className='py-1 my-1 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out hover:bg-white hover:px-1 hover:rounded-sm'>Account </li></Link>
+                <Link href='#'><li className='py-1 my-1 cursor-pointer hover:scale-105 hover:text-primary hover:font-bold transition-all ease-in-out hover:bg-white hover:px-1 hover:rounded-sm'>Logout</li></Link>
+               
             </ul>
             <div className='md:hidden cursor-pointer hover:scale-110 scale-105 transition-all ease-in-out' onClick={()=>setOpen(!open)}>
                 {open?(<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>)
--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className=' grid grid-cols-1 lg:grid-cols-2 gap-8 m-5 p-5'>
-        <div className='order-2 lg:order-1 text-justify'>chitecto aperiam enim quae vel eos ea! Neque incidunt deleniti aliquam adipisci deserunt consequuntur labore voluptatum numquam eius provident. Quidem autem mollitia earum molestias dicta officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis quo in, sed odio blanditiis non. Voluptates harum debitis natus minima tempore soluta tenetur architecto hic consectetur, aliquam labore sequi?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos cumque non temporibus ducimus ipsum quasi reiciendis soluta adipisci, enim dignissimos esse autem quae, at suscipit odio? Rem illo ipsum repellat.
-        Sint natus veniam iure esse, nisi excepturi repellendus enim et. Aut veritatis iure tempore. Suscipit voluptas quae accusantium sit magnam, provident dolores, et debitis iure incidunt optio doloremque hic inventore.
+    
+    <div className=' grid grid-cols-1 lg:grid-cols-2 gap-8 m-2 md:m-5 p-5'>
+        <div className='order-2 lg:order-1 flex justify-center items-center flex-col'>
+
+        <h2 className='text-center text-2xl font-bold pb-2 mb-2'>Heading.</h2>
+        <div className=' lg:order-1 text-justify'>chitecto aperiam enim quae vel eos ea! Neque incidunt deleniti aliquam adipisci deserunt consequuntur labore voluptatum numquam eius provident. Quidem autem mollitia earum molestias dicta officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum perspiciatis quo in, sed odio blanditiis non. Voluptates harum debitis natus minima tempore soluta tenetur architecto hic consectetur, aliquam labore sequi?
         </div>
-        <div className='order-1'>
-            <img src="heroimg.jpg" alt="img" className='rounded-lg shadow-xl'/>
+        </div>
+        <div className='order-1 flex justify-center'>
+            <img src="heroimg.jpg" alt="img" height={500} width={600} className='max-w-[100%] rounded-lg shadow-xl min-h-[350px] object-cover'/>
         </div>
     </div>
   )
